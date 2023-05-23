de6d65231b8802f409ff258fe74233621ba6cf34
Kais Salha <kaiss.salha@gmail.com>
2023-05-23 10:14:03 -0400

    fix: fix locale typings

--- src/queries/cms/categories/use-category.ts
+++ src/queries/cms/categories/use-category.ts
@@ -1,4 +1,5 @@
 import { QueryClient, useQuery } from "@tanstack/react-query";
+import { Locale, useLocale } from "providers/LocaleProvider";
 import { getQueryKey } from "../../utils/get-query-key";
 import { QUERY_IDS } from "../../utils/query-ids";
 import { CmsApi } from "../../../services/api";
@@ -7,28 +8,34 @@ interface Props {
 	slug: string;
 }
 
-export const useCategory = ({ slug }: Props) =>
-	useQuery(
-		getQueryKey([QUERY_IDS.categories, slug]),
+export const useCategory = ({ slug }: Props) => {
+	const { locale } = useLocale();
+
+	return useQuery(
+		getQueryKey([QUERY_IDS.categories, slug, locale]),
 		async () =>
 			CmsApi.getCategoryBySlug({
 				slug,
+				locale,
 			}),
 		{
 			enabled: !!slug,
 		}
 	);
+};
 
 interface Params extends Props {
 	queryClient: QueryClient;
+	locale: Locale;
 }
 
-export const getCategorySSR = async ({ queryClient, slug }: Params) => {
+export const getCategorySSR = async ({ queryClient, slug, locale }: Params) => {
 	await queryClient.prefetchQuery(
 		getQueryKey([QUERY_IDS.categories, slug]),
 		async () =>
 			CmsApi.getCategoryBySlug({
 				slug,
+				locale,
 			})
 	);
 };
