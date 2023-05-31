016614e75e935c0712acadfc542d9ba172869c3e
Kais Salha <kaiss.salha@gmail.com>
2023-05-31 14:43:48 -0400

    feat: add types for hooks return

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,5 @@
+import type { RichTextCustomElement, RichTextCustomLeaf } from "payload/types";
+
 export type Locale = "en" | "ar" | string | undefined;
+
+export type Content = RichTextCustomElement | RichTextCustomLeaf;
