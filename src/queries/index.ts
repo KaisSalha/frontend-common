50fe36fd5b32ced3fd85cf617920f77919234c11
Kais Salha <kaiss.salha@gmail.com>
2023-06-25 09:59:46 -0400

    fix: fix missing useAreas export

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,4 +1,5 @@
 export * from "./cms/categories/use-category";
 export * from "./cms/categories/use-categories";
 export * from "./profile/use-profile";
+export * from "./location/use-areas";
 export * from "../services/api/utils/api-client";
