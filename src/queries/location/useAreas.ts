ab5735518213b88f382b13eb2b42d97136ab508e
Kais Salha <kaiss.salha@gmail.com>
2023-06-25 09:53:22 -0400

    feat: add the location api and the useAreas hook

---
+++ src/queries/location/useAreas.ts
@@ -0,0 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import { LocationApi } from "../../services/api";
+import { QUERY_IDS } from "../utils/query-ids";
+
+export const useAreas = () =>
+	useQuery([QUERY_IDS.profile], () => LocationApi.getAreas());
---
+++ src/services/api/location-api.ts
@@ -0,0 +1,32 @@
+import { ApiClient } from "./utils/api-client";
+
+export interface getPolygonsResult {
+	message: string;
+	polygons: {
+		id: number;
+		geo_name: string;
+		land_area: number;
+		geom: string;
+	};
+}
+
+/**
+ * Manages requests to the location api.
+ */
+
+const serviceName = "location";
+
+ApiClient.registerService({
+	name: serviceName,
+	timeout: 10000,
+	environments: {
+		staging: "http://localhost:3011",
+		prod: "http://localhost:3011",
+	},
+});
+
+export const getAreas = async (): Promise<getPolygonsResult> => {
+	const response = await ApiClient.getClient(serviceName).get(`/`);
+
+	return response.data;
+};
