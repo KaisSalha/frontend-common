0e8bdeb4939cef6620474656a89fde8789660c67
Kais Salha <kaiss.salha@gmail.com>
2023-07-11 17:39:55 -0400

    fix: chunk census data

--- src/queries/census/use-census-by-geo-ids.ts
+++ src/queries/census/use-census-by-geo-ids.ts
@@ -1,18 +1,31 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { LocationApi } from "../../services/api";
 import { QUERY_IDS } from "../utils/query-ids";
 
-export interface useCensusByGeoIdsParams
-	extends Omit<LocationApi.getCensusByGeoIdsParams, "id"> {
-	ids: string[];
-}
+export const useCensusByGeoIds = ({
+	ids,
+}: LocationApi.getCensusByGeoIdsParams) => {
+	// Split ids into chunks of 30
+	const chunkSize = 30;
+	const idChunks = Array(Math.ceil(ids.length / chunkSize))
+		.fill(null)
+		.map((_, index) =>
+			ids.slice(index * chunkSize, (index + 1) * chunkSize)
+		);
 
-export const useCensusByGeoIds = ({ ids }: useCensusByGeoIdsParams) =>
-	useQuery(
-		[QUERY_IDS.census, ...ids],
-		() => LocationApi.getCensusByGeoIds({ ids }),
-		{
-			keepPreviousData: true,
-			enabled: ids.length > 0,
-		}
-	);
+	// Create queries for each chunk
+	const queries = idChunks.map((idChunk, index) => ({
+		queryKey: [QUERY_IDS.census, index, ...idChunk],
+		queryFn: () => LocationApi.getCensusByGeoIds({ ids: idChunk }),
+		keepPreviousData: true,
+		enabled: idChunk.length > 0,
+	}));
+
+	const results = useQueries({ queries });
+
+	return {
+		data: results.map((result) => result.data),
+		isLoading: results.some((result) => result.isLoading),
+		isError: results.some((result) => result.isError),
+	};
+};
