22d71dc1918c4f9f6b6ee4cc2c8ece0a981c939f
Kais Salha <kaiss.salha@gmail.com>
2023-08-08 18:34:03 -0400

    feat: remove legacy hook + add number utils

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./queries";
 export * from "./atoms";
 export * from "./providers";
 export * from "./types";
+export * from "./utils";
--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,5 +1,4 @@
 export * from "./cms/categories/use-category";
 export * from "./cms/categories/use-categories";
 export * from "./profile/use-profile";
-export * from "./census/use-census-by-geo-ids";
 export * from "../services/api/utils/api-client";
